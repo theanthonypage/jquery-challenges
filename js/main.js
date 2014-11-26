3de3b005b92eb5f3b743cf6370e0d9df2028b5e1
theanthonypage <apage516@gmail.com>
2014-11-25 19:07:10 -0500

    almost did things

--- js/main.js
+++ js/main.js
@@ -1,3 +1,12 @@
+
+//challenge 1 mobile nav
 $("#menu-btn").click(function(){
 	$(".nav-main").slideToggle();
+});
+
+// accordion panels
+$(".panel-content").hide();
+
+$(".panel-title").click(function(){
+	$(this).next().slideToggle();
 });
